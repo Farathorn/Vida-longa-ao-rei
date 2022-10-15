b8b14bb05f051fbac78adde615fc13c8e9c1e273
Farathorn <lucaskavalkantej@gmail.com>
2022-10-15 11:40:47 -0400

    Namespaces padronizadas

--- Program.cs
+++ Program.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 
-namespace Vida_longa_ao_rei
+namespace VLAR
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main (string[] args)
         {
             Console.WriteLine("Hello World!");
         }
