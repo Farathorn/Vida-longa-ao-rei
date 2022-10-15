16a8e244c7e0b230993381706b0909b856db5e94
Farathorn <lucaskavalkantej@gmail.com>
2022-10-15 11:41:20 -0400

    Namespaces padronizadas

--- Estruturas/ArvoreGeral.cs
+++ Estruturas/ArvoreGeral.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Arvores
+namespace VLAR.Estruturas.Arvores
 {
     public class ArvoreGeral<T> : ArvoreI<T>
     {
