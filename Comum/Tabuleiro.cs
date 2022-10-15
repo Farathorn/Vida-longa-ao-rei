5e84da6d283d3ff8d40d8a4bca57211a7256ca79
Farathorn <lucaskavalkantej@gmail.com>
2022-10-15 15:52:16 -0400

    Nova classe Casa

--- Comum/Tabuleiro.cs
+++ Comum/Tabuleiro.cs
@@ -9,20 +9,37 @@ namespace VLAR.Comum
     {
         public List<Peca> pecas { get; private set; } = new List<Peca>();
 
-        public List<List<Tuple<byte, byte>>> casas { get; private set; }
+        public List<List<Casa>> casas { get; private set; }
 
 
         public Tabuleiro(byte largura, byte altura)
         {
-            casas = new List<List<Tuple<byte, byte>>>(altura);
+            casas = new List<List<Casa>>(altura);
             for (byte i = 0; i < altura; i++)
             {
-                casas[i] = new List<Tuple<byte, byte>>(largura);
+                casas[i] = new List<Casa>(largura);
                 for (byte j = 0; j < largura; j++)
                 {
-                    casas[i][j] = new Tuple<byte, byte>(i, j);
+                    casas[i][j] = new Casa(new Tuple<byte, byte>(i, j));
                 }
             }
+        }
+    }
+
+    public class Casa
+    {
+        public Tuple<byte, byte> Coordenada { get; set; }
+        public enum Condicao
+        {
+            Ocupada,
+            Desocupada
+        }
+
+        public Condicao condicao { get; set; } = Condicao.Desocupada;
+
+        public Casa(Tuple<byte, byte> Coordenada)
+        {
+            this.Coordenada = Coordenada;
         }
     }
 }
