13adf941b18552697b2cac6ab21ad8fb5ce7d5a1
Farathorn <lucaskavalkantej@gmail.com>
2022-10-15 16:39:49 -0400

    Novo sistema de Posicoes

---
+++ Comum/Coordenada.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VLAR.Comum
+{
+    public class Posicao
+    {
+        public int x { get; private set; }
+        public int y { get; private set; }
+
+        public Posicao(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public Posicao(Posicao a, Posicao b)
+        {
+            x = a.x + b.x;
+            y = a.y + b.y;
+        }
+
+        public Posicao Somar(Posicao somante)
+        {
+            x += somante.x;
+            y += somante.y;
+
+            return this;
+        }
+
+        static public Posicao Somar(Posicao a, Posicao b)
+        {
+            return new Posicao(a.x + b.x, a.y + b.y);
+        }
+    }
+}
