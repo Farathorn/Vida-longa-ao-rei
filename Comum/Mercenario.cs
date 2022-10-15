981a79976ee19c74c66ea0d39239c8115536ac34
Farathorn <lucaskavalkantej@gmail.com>
2022-10-15 17:26:55 -0400

    Novas classes dos tipos de peças.

---
+++ Comum/Mercenario.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VLAR.Comum
+{
+    public class Mercenario : Peca
+    {
+        public Mercenario(Tabuleiro Tabuleiro, byte ID, Posicao Posicao) : base(Tabuleiro, ID, Posicao)
+        {
+
+        }
+    }
+}
